533897e11fd071e3d1de7be4b542f014a7028624
hoangvuong21 <hoangvuong.tran21@gmail.com>
2018-08-13 23:14:33 +0700

    Finish working with react and redux Handle UX and Error

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { Provider } from 'react-redux';
 
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
     <Provider store={store}>
